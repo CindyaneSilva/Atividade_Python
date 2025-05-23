f4797a822598fec2755cac4d7cdcdaee59b4fab5
Cindyane <cindyanebatista@gmail.com>
2025-05-22 23:54:56 -0300

    adicionada_atividade_12

---
+++ 12-compra.py
@@ -0,0 +1,13 @@
+#Objetivo:
+#Calcular o valor total a ser pago por um determinado numero de unidades de um produto.
+
+pdt = "pulseira"
+preco = 81.64
+qtde = 2
+
+total = preco * qtde
+
+print("Produto:" ,pdt)
+print("Preço unitário:" ,preco)
+print("Quantidade comprada:" ,qtde)
+print("Valor total: R$" ,total)
