df82ec76955b2b95554e4b6dae16bcb2a2ff1003
Cindyane <cindyanebatista@gmail.com>
2025-05-22 22:43:49 -0300

    adicionada_atividade_06

--- 06-operadores-matematicos.py
+++ 06-operadores-matematicos.py
@@ -1,9 +1,13 @@
-a = 10
-b = 3
-
-print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
-
 """
 Crie quatro novas variaveis de numeros inteiros, some elas, 
 calcule a média e exiba o resultado na tela
 """
+num1 = 15
+num2 = 20
+num3 = 16
+num4 = 21
+
+soma = num1 + num2 + num3 + num4 
+media = soma/4
+
+print("A média é ", media)
