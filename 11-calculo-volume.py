2974e05b5fd09ad7ed6137c4cb0b25fa5b426553
Cindyane <cindyanebatista@gmail.com>
2025-05-22 23:47:04 -0300

    adicionada_atividade_11

---
+++ 11-calculo-volume.py
@@ -0,0 +1,9 @@
+#Objetivo:
+#Multiplicar tres valores correspondentes as dimensoes de uma caixa e calcular seu volume.
+
+comp = 5.6
+larg = 2.7
+alt = 1.2
+
+volume = comp * larg * alt
+print("O volume da caixa é:" ,volume)
