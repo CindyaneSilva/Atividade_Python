d9ae78fcee2f307d86f39f4661866ded64547dba
Cindyane <cindyanebatista@gmail.com>
2025-05-22 23:25:50 -0300

    adicionada_atividade_08

--- 08-condicionais.py
+++ 08-condicionais.py
@@ -1,4 +1,4 @@
-idade = 18
+idade = 12
 
 if idade >= 18:
     print("Você é um adulto")
@@ -6,3 +6,9 @@ else:
     print("Você é menor de idade")
 
 #Siga o modelo acima e cria uma nova expressão condicional
+temCarteira = True
+
+if temCarteira == False and idade >= 18:
+    print("Você pode dirigir!")
+else:
+    print("Você não pode dirigir")
