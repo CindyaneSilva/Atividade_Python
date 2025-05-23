05447bfaa0ad3585c9b225ed19285d4b18479714
Cindyane <cindyanebatista@gmail.com>
2025-05-22 23:08:41 -0300

    adicionada_atividade_07

--- 07-comparacoes.py
+++ 07-comparacoes.py
@@ -1,5 +1,6 @@
-x = 10
-y = 15
-print(x > y, x < y, x == y, x != y)
-
 #Compare dois números e exiba uma frase na tela de acordo com a comparação escolhida
+x = 21
+y = 22
+
+result = x == y
+print("Os números são iguas?" ,result)
