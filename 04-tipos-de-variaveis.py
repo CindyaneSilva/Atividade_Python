1dfbd8a8faa902057f1b0566260424ffc54f73a6
Cindyane <cindyanebatista@gmail.com>
2025-05-22 22:11:15 -0300

    adicionada_atividade_04

--- 04-tipos-de-variaveis.py
+++ 04-tipos-de-variaveis.py
@@ -5,3 +5,12 @@ depois mude o tipo de cada uma e verifique o novo tipo
 
 x = 10
 y = "Dez"
+
+print("Tipo  original de x: ",type(x))
+print("Tipo  original de y: ",type(y))
+
+x = str(x)
+y = 10
+
+print("Tipo  modificado de x: ",type(x))
+print("Tipo  modificado de y: ",type(y))
