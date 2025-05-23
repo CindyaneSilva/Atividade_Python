bf7426533bdea514dffd0254ea54f90675f03e11
Cindyane <cindyanebatista@gmail.com>
2025-05-22 23:37:32 -0300

    adicionada_atividade_10

---
+++ 10-soma.py
@@ -0,0 +1,8 @@
+#Objetivo:
+#Calcular a soma de dois numeros definidos no proprio codigo e exibir o resultado.
+
+num1 = 55
+num2 = 48
+
+soma = num1 + num2
+print("A soma do números", num1, "e" ,num2, "é: " ,soma)
